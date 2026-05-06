c2823a5861eaa3a8630019bfa6548e3c914ce4b3
Sach21605in20 <sachinrc2006@gmail.com>
2026-05-06 14:24:16 +0530

    initial project structure and setup, seperate SpringProject for notification_app_be and vehicle_maintainance_scheduler

---
+++ vehicle_maintenance_scheduler/src/main/java/com/vehicle/vehicle_maintenance_scheduler/VehicleMaintenanceSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.vehicle.vehicle_maintenance_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleMaintenanceSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleMaintenanceSchedulerApplication.class, args);
+	}
+
+}
---
+++ vehicle_maintenance_scheduler/src/test/java/com/vehicle/vehicle_maintenance_scheduler/VehicleMaintenanceSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicle.vehicle_maintenance_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleMaintenanceSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
